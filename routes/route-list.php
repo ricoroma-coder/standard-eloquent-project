268f961551363a0544dc9d02ef649d65312177f3
Rico Roma <ricoroma14@gmail.com>
2020-07-28 12:35:42 -0300

    v3.0.3

--- routes/route-list.php
+++ routes/route-list.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\General\Route as Route;
+use App\Controllers\Controller;
 
-Route::get('/', 'ViewController@index');
+Route::get('/', function () {
+	$c = new Controller();
+	return $c->view('index');
+});
 
+
+// auth
 Route::get('/login', 'ViewController@login');
 Route::get('/recuperar-senha', 'ViewController@forgotPassword');
 Route::get('/cadastrar', 'ViewController@register');
